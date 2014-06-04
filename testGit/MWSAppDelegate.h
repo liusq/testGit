76de62a53f3e9f5becc6c4ed1ea6d1760c6da59b
Edward Liu <edwardliu@Edwards-Mac-mini.local>
2014-06-04 08:02:31 -0400

    Initial Commit

---
+++ testGit/MWSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MWSAppDelegate.h
+//  testGit
+//
+//  Created by Edward Liu on 2014-06-04.
+//  Copyright (c) 2014 Edward Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MWSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
